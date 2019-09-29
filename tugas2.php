88c8f83de0b1d306becb207e7086622f034c3f1e
xaber17 <55624860+xaber17@users.noreply.github.com>
2019-09-29 22:05:18 +0700

    Add files via upload

---
+++ tugas2.php
@@ -0,0 +1,12 @@
+<?php
+    for($x=1; $x<=100; $x++)
+        {
+            if($x % 2 == 0)	{
+            	echo "$x = Bilangan Genap";
+            }
+            else {
+            	echo "$x = Bilangan Ganjil";
+            }
+            echo "<br>";
+        }
+?>
